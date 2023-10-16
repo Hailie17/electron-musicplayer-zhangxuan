85cd754862899cb870d85f915ec84b2088cfaad3
Hailie <wang_junnan@hotmail.com>
2023-10-16 10:16:03 +0800

    feat: 创建窗口类，设置config

--- main.js
+++ main.js
@@ -1,17 +1,35 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 
+class AppWindow extends BrowserWindow {
+  constructor(config, location) {
+    const basicConfig = {
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+      },
+    };
+    const finalConfig = { ...basicConfig, ...config };
+    super(finalConfig);
+    this.loadFile(location);
+    this.once("ready-to-show", () => {
+      this.show();
+    });
+  }
+}
+
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
-  });
-  mainWindow.loadFile("index.html");
-  ipcMain.on("message", (event, arg) => {
-    console.log(arg);
-    event.sender.send("reply", "reply from main");
+  const mainWindow = new AppWindow({}, "./renderer/index.html");
+
+  ipcMain.on("add-music-window", () => {
+    const addWindow = new AppWindow(
+      {
+        width: 600,
+        height: 400,
+        parent: mainWindow,
+      },
+      "./renderer/add.html"
+    );
   });
 });
